5e9db981b95402fde640a3292b253b728cd29f64
Rafael Mendonça França <rafaelmfranca@gmail.com>
2011-01-27 12:40:41 -0300

    Gerada a aplicação Rails sem o ActiveRecord

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MongoidExample::Application.config.secret_token = '81a8258d0d34ab100570da4c94938457c8ed96b50c09a72321a39634f32a0b25a5dc1cd643f9b79543dc445cea26d1d243adb62c9068197a0dfb571fe864ece2'
